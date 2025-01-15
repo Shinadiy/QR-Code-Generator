54ca381544fa7091a5ca79afcc5ed86c9f970e05
Sheena Marie Dinaguit <125778035+Shinadiy@users.noreply.github.com>
2025-01-15 23:56:53 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function generateQRCode() {
+    const value = document.getElementById('qr-code-value').value;
+
+    if (!value.trim()) {
+        alert("Please enter a valid text or URL.");
+        return;
+    }
+
+    const qrCode = new QRious({
+        value: value,
+        size: 400,
+        level: 'H'
+    });
+
+    document.getElementById('qr-code-image').src = qrCode.toDataURL();
+}
